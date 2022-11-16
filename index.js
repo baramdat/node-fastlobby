e38d3b3050f7ec5bdb26b5fbafaf488d3a384485
baramdat <baramdat@gmail.com>
2022-11-16 13:47:01 +0500

    index files

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const express = require('express');
 
 const app = express();
-var server = require('http').createServer(app)
-
-server.listen(80, () => {
-    console.log('Server is running');
-});
 
 const { proxy, scriptUrl } = require('rtsp-relay')(app);
 
@@ -17,17 +12,17 @@ const handler = proxy({
     additionalFlags: ['-q', '8']
 });
 
-// app.listen(2000, () => {
-//     console.log('App is running');
-// });
+app.listen(80, () => {
+    console.log('App is running');
+});
 
-server.get("/url", (req, res, next) => {
+app.get("/url", (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });
 
 // the endpoint our RTSP uses
 // app.ws('/api/stream', handler);
-server.ws('/api/stream/:chID', (ws, req) =>
+app.ws('/api/stream/:chID', (ws, req) =>
   proxy({
     url: `rtsp://admin:troiano10!@68.195.234.210:3067/chID=${req.params.chID}&streamType=main&linkType=tcp`,
     // if your RTSP stream need credentials, include them in the URL as above
@@ -38,7 +33,7 @@ server.ws('/api/stream/:chID', (ws, req) =>
 );
 
 // this is an example html page to view the stream
-server.get('/streaming/:chID', (req, res) =>
+app.get('/streaming/:chID', (req, res) =>
     res.send(`
     <canvas id='canvas' style="width:100%;height:100%"></canvas>
 
