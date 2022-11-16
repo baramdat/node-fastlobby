9a00749e24515ef16119e49b5f2b231bd8754a50
baramdat <baramdat@gmail.com>
2022-11-16 13:32:40 +0500

    index files

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
-const http = require('http');
+const express = require('express');
 
-const hostname = 'https://node-fastlobby.vercel.app';
-const port = 3000;
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+const { proxy, scriptUrl } = require('rtsp-relay')(app);
+
+const handler = proxy({
+    url: `rtsp://admin:troiano10!@68.195.234.210:3067/chID=2&streamType=main&linkType=tcp`,
+    // if your RTSP stream need credentials, include them in the URL as above
+    verbose: false,
+    transport: 'tcp',
+    additionalFlags: ['-q', '8']
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(2000, () => {
+    console.log('App is running');
 });
+
+app.get("/url", (req, res, next) => {
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+});
+
+// the endpoint our RTSP uses
+// app.ws('/api/stream', handler);
+app.ws('/api/stream/:chID', (ws, req) =>
+  proxy({
+    url: `rtsp://admin:troiano10!@68.195.234.210:3067/chID=${req.params.chID}&streamType=main&linkType=tcp`,
+    // if your RTSP stream need credentials, include them in the URL as above
+    verbose: false,
+    transport: 'tcp',
+    additionalFlags: ['-q', '10']
+  })(ws),
+);
+
+// this is an example html page to view the stream
+app.get('/streaming/:chID', (req, res) =>
+    res.send(`
+    <canvas id='canvas' style="width:100%;height:100%"></canvas>
+
+    <script src='${scriptUrl}'></script>
+    <script>
+        loadPlayer({
+        url: 'ws://localhost:2000/api/stream/${req.params.chID}',
+        canvas: document.getElementById('canvas')
+        });
+    </script>
+    `),
+);
---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+
+const app = express();
+
+const { proxy, scriptUrl } = require('rtsp-relay')(app);
+
+const handler = proxy({
+    url: `rtsp://admin:troiano10!@68.195.234.210:3067/chID=2&streamType=main&linkType=tcp`,
+    // if your RTSP stream need credentials, include them in the URL as above
+    verbose: false,
+    transport: 'tcp',
+    additionalFlags: ['-q', '8']
+});
+
+app.listen(2000, () => {
+    console.log('App is running');
+});
+
+app.get("/url", (req, res, next) => {
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+});
+
+// the endpoint our RTSP uses
+// app.ws('/api/stream', handler);
+app.ws('/api/stream/:chID', (ws, req) =>
+  proxy({
+    url: `rtsp://admin:troiano10!@68.195.234.210:3067/chID=${req.params.chID}&streamType=main&linkType=tcp`,
+    // if your RTSP stream need credentials, include them in the URL as above
+    verbose: false,
+    transport: 'tcp',
+    additionalFlags: ['-q', '10']
+  })(ws),
+);
+
+// this is an example html page to view the stream
+app.get('/streaming/:chID', (req, res) =>
+    res.send(`
+    <canvas id='canvas' style="width:100%;height:100%"></canvas>
+
+    <script src='${scriptUrl}'></script>
+    <script>
+        loadPlayer({
+        url: 'ws://localhost:2000/api/stream/${req.params.chID}',
+        canvas: document.getElementById('canvas')
+        });
+    </script>
+    `),
+);
